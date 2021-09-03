257f3c9985a09cb97dc55faa5b22698bf844fee7
VictorVenegas07 <vvenegas@unicesar.edu.co>
2021-09-02 23:18:47 -0500

    Comit inicial

---
+++ registro/registro.js
@@ -0,0 +1,52 @@
+function Calcular() {
+    var op = document.getElementById("opcion").value;
+    var idenficacion = document.getElementById("idenficacion").value;
+    var nombre = document.getElementById("nombre").value;
+    var edad = document.getElementById("edad").value;
+    var resultado;
+    if (op == "Femenino") {
+        //  resultado =  (220 - parseInt(edad)) /10 ;
+        resultado = Femenino(edad);
+    } else if ("Masculino") {
+        // resultado =  (210 - parseInt(edad)) /10 ;
+        resultado = Masculino(edad);
+    }
+
+    parent(document.getElementById("resultado").value = resultado)
+    guardar(idenficacion, nombre, op, edad, resultado);
+}
+
+function Masculino(edad) {
+    var resultado = (210 - parseInt(edad)) / 10;
+    return resultado;
+}
+function Femenino(edad) {
+    var resultado = (220 - parseInt(edad)) / 10;
+    return resultado;
+}
+function consultar(){
+    guardar(idenficacion, nombre, op, edad, resultado);
+}
+
+function guardar(idenficacion, nombre, op, edad, resultado) {
+    var operacionew = { "idenficacion": idenficacion, "nombre": nombre, "op": op, "edad": edad, "resultado": resultado };
+    alert(idenficacion);
+    var operaciones = [] ;
+    if (JSON.parse(localStorage.getItem('dato'))!= null) {
+        operaciones = JSON.parse(localStorage.getItem('dato'));
+    }
+    operaciones.push(operacionew);
+    localStorage.setItem('dato', JSON.stringify(operaciones));
+    var pval = "";
+    operaciones.array.forEach(element => {
+        pval += "<tr>"
+             pval += "<td>" + element.idenficacion + "</td>"
+             pval += "<td>" + element.nombre + "</td>"
+             pval += "<td>" + element.op + "</td>"
+             pval += "<td>" + element.edad + "</td>"
+             pval += "<td>" + element.resultado + "</td>"
+           pval += "</tr>"
+
+    });
+    document.getElementById("tdCuerpo").innerHTML = pval;
+}
